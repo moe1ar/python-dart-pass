8571dbab8bd64a199321d5def59ab408ff381ed7
Mohammed Abdulrasool <96358396+moe1ar@users.noreply.github.com>
2022-04-07 16:22:45 +0300

    Create dart-pass-1.dart

---
+++ dart-pass/dart-pass-1.dart
@@ -0,0 +1,9 @@
+main() {
+  var hour = DateTime.now().hour;
+  print(hour);
+}
+
+/*
+We must specify the data type and the data type 'const' cannot be specified for a variable value such as time,
+so we must set the data type of a variable such as 'var'.
+*/
