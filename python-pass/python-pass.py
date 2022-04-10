1ef2055d0b6babc97fd24dda73eda935e76ed4b8
Mohammed Abdulrasool <96358396+moe1ar@users.noreply.github.com>
2022-04-10 14:04:01 -0400

    Update python-pass.py

--- python-pass/python-pass.py
+++ python-pass/python-pass.py
@@ -2,16 +2,18 @@ def EVEN_ODD():
 	n=int(input("Enter the x value :"))
 	num=[]
 	for i in range (n):
-		number=int(input())
-		num.append(number)
-	for i in num:
-		if 0<i<=10:
-			if i%2==0:
-				print(i, " is even")
-			elif i%2!=0:
-				print(i," is odd ")
-			else:
-				print(" is zero")
+                if 0<i<=10:
+			number=int(input())
+			num.append(number)
 		else:
 			print(i ,"is out the range ")
+	for i in num:
+		
+		if i%2==0:
+			print(i, " is even")
+		elif i%2!=0:
+			print(i," is odd ")
+		else:
+			print(" is zero")
+	
 EVEN_ODD()
